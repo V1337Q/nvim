57742b9d1120600f130e79f4f58b67a872b8642c
VerumQuaerite <kaihngje.fixedgear49@gmail.com>
2025-01-22 10:22:09 -0500

    Feat: Added cusstom nvimtree lua config

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -3,10 +3,8 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("nvim-tree").setup {
-      -- Your nvim-tree configuration
     }
 
-    -- Apply custom highlights
     local get_theme_tb = require("base46").get_theme_tb
     local colors = get_theme_tb "base_30"
     local theme_type = get_theme_tb "type"
@@ -14,6 +12,5 @@ return {
 
     vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { fg = colors.folder_bg })
     vim.api.nvim_set_hl(0, "NvimTreeWinSeparator", { fg = colors.black, bg = colors.black })
-    -- Add more highlight overrides as needed
   end,
 }
